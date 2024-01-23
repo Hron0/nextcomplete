829436713ce49e42c652f04e001b194be9847699
Hron0 <sasha1254370@gmail.com>
2024-01-23 18:43:41 +0300

    Meaningless changes lol

---
+++ app/lib/definitions.ts
@@ -0,0 +1,6 @@
+export type UserType = {
+    id: String;
+    name: String;
+    email:String;
+    password: String;
+}
---
+++ app/ui/dashboard/Nav-Link.tsx
@@ -0,0 +1,2 @@
+import { UserGroupIcon, HomeIcon, DocumentDuplicateIcon } from "@heroicons/react/24/outline";
+import Link from 'next/link'
